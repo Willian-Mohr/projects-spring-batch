40301a7db013c99597728ad19690f58ab154f145
Willian Mohr <willianbmohr@gmail.com>
2022-04-10 13:58:20 -0300

    Refatoração do projeto Spring Batch com base no chunk

--- spring-bacth-chunk-based/src/main/java/br/com/wohr/springbacthchunkbased/step/ImprimeParOuImparStepConfig.java
+++ spring-bacth-chunk-based/src/main/java/br/com/wohr/springbacthchunkbased/step/ImprimeParOuImparStepConfig.java
@@ -1,14 +1,10 @@
-package br.com.wohr.springbacthchunkbased;
+package br.com.wohr.springbacthchunkbased.step;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.function.FunctionItemProcessor;
 import org.springframework.batch.item.support.IteratorItemReader;
@@ -17,34 +13,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableBatchProcessing
-public class ParImparBatchConfig {
-	
-	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
+public class ImprimeParOuImparStepConfig {
 	
 	@Autowired
 	private StepBuilderFactory stepBuilderFactory;
 	
 	@Bean
-	public Job imprimeParImparJob() {
-		return jobBuilderFactory
-				.get("imprimeParImparJob")
-				.start(imprimeParImparStep())
-				.incrementer(new RunIdIncrementer())
-				.build();
-	}
-	
 	public Step imprimeParImparStep() {
 		return stepBuilderFactory
 				.get("imprimeParImparStep")
-				.<Integer, String>chunk(1)
+				.<Integer, String>chunk(10)
 				.reader(contaAteDezReader())
 				.processor(parOuImparProcessor())
 				.writer(ImprimeWriter())
 				.build();
 	}
-
+	
 	private IteratorItemReader<Integer> contaAteDezReader() {
 		List<Integer> numerosDeUmAteDez = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 		return new IteratorItemReader<>(numerosDeUmAteDez.iterator());
