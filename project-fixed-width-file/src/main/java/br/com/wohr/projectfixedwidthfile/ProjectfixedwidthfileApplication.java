317aee480f7f45ccb321120c927df469c9dae86f
Willian Mohr <willianbmohr@gmail.com>
2022-04-10 20:21:27 -0300

    Criando novo projeto para ler arquivos com largura fixa

---
+++ project-fixed-width-file/src/main/java/br/com/wohr/projectfixedwidthfile/ProjectfixedwidthfileApplication.java
@@ -0,0 +1,13 @@
+package br.com.wohr.projectfixedwidthfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectfixedwidthfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectfixedwidthfileApplication.class, args);
+	}
+
+}
---
+++ project-fixed-width-file/src/test/java/br/com/wohr/projectfixedwidthfile/ProjectfixedwidthfileApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wohr.projectfixedwidthfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectfixedwidthfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
