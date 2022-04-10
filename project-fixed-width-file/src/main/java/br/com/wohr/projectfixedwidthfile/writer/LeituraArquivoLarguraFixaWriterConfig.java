f3f09762fb179dcf126d7ed044ebebc06dd3bef6
Willian Mohr <willianbmohr@gmail.com>
2022-04-10 20:21:27 -0300

    Criando novo projeto para ler arquivos com largura fixa

---
+++ project-fixed-width-file/src/main/java/br/com/wohr/projectfixedwidthfile/writer/LeituraArquivoLarguraFixaWriterConfig.java
@@ -0,0 +1,19 @@
+package br.com.wohr.projectfixedwidthfile.writer;
+
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.wohr.projectfixedwidthfile.domain.Cliente;
+
+@Configuration
+public class LeituraArquivoLarguraFixaWriterConfig {
+	
+	@Bean
+	public ItemWriter<Cliente> leituraArquivoLarguraFixaWriter() {
+		
+		return items -> items.forEach(System.out::println);
+		
+	}
+	
+}
