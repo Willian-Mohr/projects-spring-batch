05250a5b487d04be54cde2cdf4c8dbbfbefffdc9
Willian Mohr <willianbmohr@gmail.com>
2022-04-10 21:10:26 -0300

    Criando novo projeto para ler arquivos com delimitador

---
+++ project-file-with-delimiter/src/main/java/br/com/wohr/projectfilewithdelimiter/step/LeituraArquivoDelimitadoStepConfig.java
@@ -0,0 +1,32 @@
+package br.com.wohr.projectfilewithdelimiter.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.wohr.projectfilewithdelimiter.domain.Cliente;
+
+@Configuration
+public class LeituraArquivoDelimitadoStepConfig {
+	
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Step leituraArquivoDelimitadoStep(ItemReader<Cliente> leituraArquivoDelimitadoReader,
+			ItemWriter<Cliente> leituraArquivoDelimitadoWriter) {
+		
+		return stepBuilderFactory
+				.get("leituraArquivoDelimitadoStep")
+				.<Cliente, Cliente>chunk(1)
+				.reader(leituraArquivoDelimitadoReader)
+				.writer(leituraArquivoDelimitadoWriter)
+				.build();
+		
+	}
+	
+}
---
+++ project-file-with-delimiter/src/test/java/br/com/wohr/projectfilewithdelimiter/ProjectfilewithdelimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wohr.projectfilewithdelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectfilewithdelimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
