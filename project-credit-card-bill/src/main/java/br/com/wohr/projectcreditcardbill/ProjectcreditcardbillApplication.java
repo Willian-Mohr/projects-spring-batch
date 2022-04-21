c27d686aeea885c9c9504cdf2c7db8462af50fd9
Willian Mohr <willianbmohr@gmail.com>
2022-04-21 16:34:46 -0300

    Criado projeto responsável por gerar fatura de cartão de crédito

---
+++ project-credit-card-bill/src/main/java/br/com/wohr/projectcreditcardbill/ProjectcreditcardbillApplication.java
@@ -0,0 +1,13 @@
+package br.com.wohr.projectcreditcardbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectcreditcardbillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectcreditcardbillApplication.class, args);
+	}
+
+}
