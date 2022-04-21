531e5ad43463f2c2adf5dba4d714de276d8be585
Willian Mohr <willianbmohr@gmail.com>
2022-04-21 13:29:07 -0300

    Criado projeto para realizar migração de dados através de arquivo delimitado

---
+++ data-migration-project/src/main/java/br/com/wohr/datamigrationproject/DatamigrationprojectApplication.java
@@ -0,0 +1,13 @@
+package br.com.wohr.datamigrationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamigrationprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamigrationprojectApplication.class, args);
+	}
+
+}
---
+++ data-migration-project/src/test/java/br/com/wohr/datamigrationproject/DatamigrationprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wohr.datamigrationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamigrationprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
