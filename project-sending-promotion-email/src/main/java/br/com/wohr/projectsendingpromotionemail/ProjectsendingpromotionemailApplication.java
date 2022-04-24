02af038b562a5461776235c35a4eb95f165fa8b6
Willian Mohr <willianbmohr@gmail.com>
2022-04-24 16:21:28 -0300

    Criando projeto para envio de email promocionais

---
+++ project-sending-promotion-email/src/main/java/br/com/wohr/projectsendingpromotionemail/ProjectsendingpromotionemailApplication.java
@@ -0,0 +1,13 @@
+package br.com.wohr.projectsendingpromotionemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsendingpromotionemailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsendingpromotionemailApplication.class, args);
+	}
+
+}
---
+++ project-sending-promotion-email/src/test/java/br/com/wohr/projectsendingpromotionemail/ProjectsendingpromotionemailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wohr.projectsendingpromotionemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsendingpromotionemailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
